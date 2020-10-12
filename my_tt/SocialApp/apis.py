e591e7e574b8c862d9507beed74d63bbe173b7e0
15270823532 <70007690+15270823532@users.noreply.github.com>
2020-10-12 18:14:56 +0800

    Develop (#12)
    * 创建了用户模块
    
    * 创建用户模型成功
    
    * 完成给用户发送短信验证码
    
    * 提交验证码，登录需求中间件
    
    * 生成用户资料模型，根据用户Id查找其对应的资料
    
    * 完成了更新个人资料接口
    
    * 完成了七牛云保存头像
    
    * 启用redis缓存
    
    * 完成异步发短信功能
    
    * 1
    
    * 准备好了Social的接口
    
    * 一口气完成了返回功能和查看喜欢过我的人功能
    
    * 完成了查看好友接口
    
    * 完成了VIP功能模块
    
    * 完成了VIP功能模块 (#4)
    
    Co-authored-by: 黄新发 <15270823532@qq.com>
    
    * print-fage
    
    * 1
    
    * 1
    
    * 将session的UID绑定到request.uid上
    
    Co-authored-by: 黄新发 <15270823532@qq.com>

--- my_tt/SocialApp/apis.py
+++ my_tt/SocialApp/apis.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from libs.http import render_json
 from SocialApp import logics
 from VipApp.logics import perm_required
@@ -6,48 +5,42 @@ from VipApp.logics import perm_required
 
 def rcmd_users(request):
     '''获取推荐用户'''
-    user_id = request.session.get('user_id')
-    users = logics.rcmd(user_id)
+    users = logics.rcmd(request.uid)
     users_list = [user.to_dict() for user in users]
     return render_json(data=users_list)
 
 
 def like(request):
     '''喜欢（右滑）'''
-    user_id = int(request.session.get('user_id'))
     sid = int(request.POST.get('sid'))
-    is_matched = logics.like_someone(user_id, sid)
+    is_matched = logics.like_someone(request.uid, sid)
     return render_json(data={'is_match': is_matched})
 
 @perm_required('superlike')
 def superlike(request):
     '''超级喜欢（上滑）'''
-    user_id = int(request.session.get('user_id'))
     sid = int(request.POST.get('sid'))
-    is_matched = logics.superlike_someone(user_id, sid)
+    is_matched = logics.superlike_someone(request.uid, sid)
     return render_json(data={'is_match': is_matched})
 
 
 def dislike(request):
     '''不喜欢（左滑）'''
-    user_id = int(request.session.get('user_id'))
     sid = int(request.POST.get('sid'))
-    logics.dislike_someone(user_id, sid)
+    logics.dislike_someone(request.uid, sid)
     return render_json()
 
 
 @perm_required('rewind')
 def rewind(request):
     '''反悔（单独按钮）'''
-    user_id = int(request.session.get('user_id'))
-    logics.rewind_last_swiped(user_id)
+    logics.rewind_last_swiped(request.uid)
     return render_json()
 
 @perm_required('show_fans')
 def show_fans(request):
     '''查看喜欢过我的人'''
-    user_id = int(request.session.get('user_id'))
-    fans = logics.show_like_me(user_id)
+    fans = logics.show_like_me(request.uid)
     fans_data = [fan.to_dict() for fan in fans]
     return render_json(data=fans_data)
 
@@ -57,80 +50,3 @@ def show_friends(request):
     all_friends = logics.show_my_friend(uid)
     friends_list = [f.to_dict() for f in all_friends]
     return render_json(data=friends_list)
-=======
-from libs.http import render_json
-from SocialApp import logics
-<<<<<<< develop
-from VipApp.logics import perm_required
-=======
-from common import errors
->>>>>>> master
-
-
-def rcmd_users(request):
-    '''获取推荐用户'''
-    user_id = request.session.get('user_id')
-    users = logics.rcmd(user_id)
-    users_list = [user.to_dict() for user in users]
-    return render_json(data=users_list)
-
-
-def like(request):
-    '''喜欢（右滑）'''
-    user_id = int(request.session.get('user_id'))
-    sid = int(request.POST.get('sid'))
-    is_matched = logics.like_someone(user_id, sid)
-    return render_json(data={'is_match': is_matched})
-
-<<<<<<< develop
-@perm_required('superlike')
-=======
-
->>>>>>> master
-def superlike(request):
-    '''超级喜欢（上滑）'''
-    user_id = int(request.session.get('user_id'))
-    sid = int(request.POST.get('sid'))
-    is_matched = logics.superlike_someone(user_id, sid)
-    return render_json(data={'is_match': is_matched})
-
-
-def dislike(request):
-    '''不喜欢（左滑）'''
-    user_id = int(request.session.get('user_id'))
-    sid = int(request.POST.get('sid'))
-    logics.dislike_someone(user_id, sid)
-    return render_json()
-<<<<<<< develop
-print('ss')
-
-@perm_required('rewind')
-=======
-
-
->>>>>>> master
-def rewind(request):
-    '''反悔（单独按钮）'''
-    user_id = int(request.session.get('user_id'))
-    logics.rewind_last_swiped(user_id)
-    return render_json()
-
-<<<<<<< develop
-@perm_required('show_fans')
-=======
-
->>>>>>> master
-def show_fans(request):
-    '''查看喜欢过我的人'''
-    user_id = int(request.session.get('user_id'))
-    fans = logics.show_like_me(user_id)
-    fans_data = [fan.to_dict() for fan in fans]
-    return render_json(data=fans_data)
-
-
-def show_friends(request):
-    uid = request.uid
-    all_friends = logics.show_my_friend(uid)
-    friends_list = [f.to_dict() for f in all_friends]
-    return render_json(data=friends_list)
->>>>>>> 08352b3758e7a28c60a8e8297c32a8b19b54dab2
